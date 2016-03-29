0117d08a819fabd3b3a8277e83bde26fe685123c
smartytwiti <marwen109@gmail.com>
2016-03-29 07:36:59 +0100

    added http-server for serving static files

---
+++ pow-http-server.js
@@ -0,0 +1,33 @@
+
+var http = require("http");
+var fs = require("fs");
+
+var server = http.createServer(function (req, res) {
+    var possibleFilename = req.url.slice(1) || "dummy";
+
+    fs.stat(possibleFilename, function (err, stats) {
+
+        var fileBuffer;
+
+        if (!err && stats.isFile()) {
+
+            var fileExtension = possibleFilename.split('.');
+
+            fileExtension = fileExtension[fileExtension.length - 1];
+
+            console.log("Sending file: %s", possibleFilename);
+            fileBuffer = fs.readFileSync(possibleFilename);
+            res.writeHead(200, { 'Content-Type': 'text/' + fileExtension });
+        }
+        else {
+            console.log("Route %s, replacing with index.html", possibleFilename);
+            fileBuffer = fs.readFileSync("index.html");
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+        }
+
+        res.write(fileBuffer);
+        res.end();
+    });
+});
+
+server.listen(8080, function () { return console.log("Listening on 8080"); });
